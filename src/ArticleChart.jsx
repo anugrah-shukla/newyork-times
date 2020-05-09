5a6e2294edf6d8374a2e99317886bb37f5f83fb9
Muskan Chitara <Muskan Chitara>
2020-05-09 20:10:52 +0530

    removed legend in chart

--- src/ArticleChart.jsx
+++ src/ArticleChart.jsx
@@ -6,7 +6,6 @@ import {
   ValueAxis,
   AreaSeries,
   Title,
-  Legend,
 } from '@devexpress/dx-react-chart-material-ui';
 
 import { ArgumentScale, Animation } from '@devexpress/dx-react-chart';
@@ -14,15 +13,16 @@ import { scalePoint } from 'd3-scale';
 import { withStyles } from '@material-ui/core/styles';
 
 const data = [
-  { year: '50', android: 67225 },
-  { year: '110', android: 179873 },
-  { year: '100', android: 310088 },
-  { year: '150', android: 539318 },
-  { year: '140', android: 539318 },
-  { year: '200', android: 539318 },
-  { year: '250', android: 539318 },
-  { year: '255', android: 539318 },
-  { year: '300', android: 539318 },
+  { year: '2011', artcless: 50 },
+  { year: '2012', artcles: 50 },
+  { year: '2013', artcles: 110 },
+  { year: '2014', artcles: 100 },
+  { year: '2015', artcles: 150 },
+  { year: '2016', artcles: 140 },
+  { year: '2017', artcles: 200 },
+  { year: '2018', artcles: 250 },
+  { year: '2019', artcles: 255 },
+  { year: '2020', artcles: 300 },
 ];
 
 const chartRootStyles = {
@@ -30,40 +30,11 @@ const chartRootStyles = {
     paddingRight: '20px',
   },
 };
-const legendStyles = {
-  root: {
-    display: 'flex',
-    margin: 'auto',
-    flexDirection: 'row',
-  },
-};
-const legendLabelStyles = theme => ({
-  label: {
-    paddingTop: theme.spacing(1),
-  },
-});
-const legendItemStyles = {
-  item: {
-    flexDirection: 'column',
-  },
-};
 
 const ChartRootBase = ({ classes, ...restProps }) => (
   <Chart.Root {...restProps} className={classes.chart} />
 );
-const LegendRootBase = ({ classes, ...restProps }) => (
-  <Legend.Root {...restProps} className={classes.root} />
-);
-const LegendLabelBase = ({ classes, ...restProps }) => (
-  <Legend.Label {...restProps} className={classes.label} />
-);
-const LegendItemBase = ({ classes, ...restProps }) => (
-  <Legend.Item {...restProps} className={classes.item} />
-);
 const ChartRoot = withStyles(chartRootStyles, { name: 'ChartRoot' })(ChartRootBase);
-const LegendRoot = withStyles(legendStyles, { name: 'LegendRoot' })(LegendRootBase);
-const LegendLabel = withStyles(legendLabelStyles, { name: 'LegendLabel' })(LegendLabelBase);
-const LegendItem = withStyles(legendItemStyles, { name: 'LegendItem' })(LegendItemBase);
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
@@ -87,20 +58,13 @@ export default class Demo extends React.PureComponent {
           <ValueAxis />
 
           <AreaSeries
-            name="Android"
-            valueField="android"
+            name=""
+            valueField="artcles"
             argumentField="year"
           />
           <Animation />
-          <Legend
-            position="bottom"
-            rootComponent={LegendRoot}
-            itemComponent={LegendItem}
-            labelComponent={LegendLabel}
-          />
           <Title
-            text="NUMBER OF ARTICLES PUBLISHED FOR “Elections”"
-          />
+            text="NUMBER OF artclesS PUBLISHED FOR “Elections”"/>
         </Chart>
       </Paper>
     );
