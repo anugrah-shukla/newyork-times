cb6d343929b5e2dfae03d29c9042b99bdaeed952
Anugrah Shukla <anugrah.shukla@docsapp.in>
2020-05-10 22:38:52 +0530

    pagination

--- src/ArticleTable.jsx
+++ src/ArticleTable.jsx
@@ -6,7 +6,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
+import Pagination from '@material-ui/lab/Pagination';
 import TableRow from '@material-ui/core/TableRow';
 import axios from 'axios';
 
@@ -52,23 +52,7 @@ function createData(published_date, headline, summary, url, source) {
   return { published_date, headline, summary, url, source };
 }
 
-var rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
+var rows = [];
 
 const useStyles = makeStyles({
   root: {
@@ -88,6 +72,7 @@ export default  function StickyHeadTable(args) {
   
   const handleChangePage = async (event, newPage) => {
     console.log("new page is: ",newPage);
+    newPage--;
     //update rows variable
     let response = await axios({
       method: 'get',
@@ -97,18 +82,21 @@ export default  function StickyHeadTable(args) {
     var data = response.data.response.docs;
     rows=[];
     for(var i=0;i<data.length;i++){
-      var d = createData(data[i].pub_date,data[i].headline.main,data[i].abstract,data[i].web_url,data[i].source);
+      var d = createData(data[i].pub_date.split('T')[0],data[i].headline.main,data[i].abstract.substring(0,120)+"...",<a href={data[i].web_url} target="_blank">Go to article</a>,data[i].source);
       console.log("pushing: ",d);
-      rows.push(d);
+      rows.push(d);   
     }
     setPage(newPage);
   };
-
+  if(rows.length==0){
+    rows= args.defaultRows;
+  }
+  console.log("assigning default values to rows");
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-  rows= args.defaultRows;
+  
   return (
     <Paper className={classes.root} style={{width:'96%',marginTop:"2%",marginLeft:'3.5%',height:'100%'}}>
       <TableContainer className={classes.container}>
@@ -144,15 +132,7 @@ export default  function StickyHeadTable(args) {
           </TableBody>
         </Table>
       </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage}
-      />
+      <Pagination count={100} variant="outlined" shape="rounded" onChange={handleChangePage} style={{marginTop:'1%'}}/>
     </Paper>
   );
 }
