621f60568031caa79e8fe6d6499cdaf6f8ca68de
Anugrah Shukla <anugrah.shukla@docsapp.in>
2020-05-10 16:51:23 +0530

    search button

--- src/App.js
+++ src/App.js
@@ -7,22 +7,30 @@ import SideBar from './SideBar';
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {activeButton:"zero-state"};
+    this.state = {
+      activeButton:"zero-state",
+      searchKeyword:''
+    };
   }
   changeButton =(activeButton)=>{
+    console.log("inside app.js  change button function: ",activeButton);
     this.setState({activeButton:activeButton});
+  }
+  setSearchKeyword=(searchKeyword,activeButton)=>{
+    console.log("inside app.js set search keyword function: ",searchKeyword);
+    this.setState({searchKeyword:searchKeyword,activeButton:activeButton});
   }
   render() {
     return (
       <Container fluid className="MainPage">
     <Row className="top-bar">
       <Col className="logo-bar" md={{span: 3}}><img alt='logo' src='nytimes.png' width="80%"></img></Col>
-      <Col className="search-bar" md={{span: 9}}><SearchBar/></Col>
+      <Col className="search-bar" md={{span: 9}}><SearchBar setSearchKeyword={this.setSearchKeyword}/></Col>
     </Row>
     <Row className="bottom-layout">
       <Col className="side-bar-layout"  md={{span: 3}}> <SideBar  changeButton={this.changeButton}/></Col>
       <Col className="content-layout" md={{span: 9}}>
-        <ContentLayout contentType={this.state.activeButton}/>
+        <ContentLayout contentType={this.state.activeButton} searchkeyword={this.state.searchKeyword}/>
       </Col>
     </Row>
   </Container>
--- src/ContentLayout.js
+++ src/ContentLayout.js
@@ -9,19 +9,24 @@ class ContentLayout extends React.Component {
         this.state = {contentType: "Dashboard"};
       }
     func = ()=>{
-        if(this.props.contentType==="Dashboard"){
+        if(this.props.contentType==="search-results"){
+          return (<div style={{height:'100%'}}>
+                    <div style={{height:'90%'}}><ArticleTable searchkeyword={this.props.searchkeyword}/></div> 
+                    {/* <div style={{height:'40%',marginTop:'4%'}}><Paper/></div> */}
+                 </div>);
+        }
+        else if(this.props.contentType==="Dashboard"){
           return <img alt="zero-state-img" src="zero-state.png" height= "35%" style={{marginTop:'30%',marginLeft:'40%'}}></img>
         } else if(this.props.contentType==="Articles"){
-          return (<div style={{height:'100%'}}>
-                    <div style={{height:'40%'}}><ArticleTable/></div> 
-                    <div style={{height:'40%',marginTop:'4%'}}><Paper/></div>
-                 </div>);
+          return <h4>Articles here</h4>;
         }else if(this.props.contentType==="Analytics"){
           return  <h4>Analytics here</h4>;
         }else if(this.props.contentType==="Messages"){
           return  <h4>Messages here</h4>
         }else if(this.props.contentType==="Calendar"){
           return <h4>Calendar here</h4>
+        }else{
+          return <h4>no show here</h4>
         }
     }
     render(){
